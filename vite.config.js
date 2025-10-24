ee8913e8d78bb5734b5c5a7741cedac931397244
cleliepradeau <clelie.pradeau@gmail.com>
2025-10-24 12:46:50 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "sae3.01", // ⚠️ Remplacer par le nom de votre dépôt GitHub
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        inscription: resolve(__dirname, "pages/inscription.html"),
+        produit_filtre: resolve(__dirname, "pages/produit_filtre.html"),
+        produit: resolve(__dirname, "pages/produit.html"),
+      },
+    },
+  },
 });
